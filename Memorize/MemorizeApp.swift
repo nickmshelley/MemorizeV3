848489bea1124cf3ac520890fd05640706af5b13
Nick Shelley <nicknick@churchofjesuschrist.org>
2024-03-16 08:37:42 -0600

    New project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nick Shelley on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
